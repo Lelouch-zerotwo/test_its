20f3e826de6508c871ea8c0ecb9566e1126e0423
AzureAD\AngeloBachettiCorsoI <bachetti.angelo005@gmail.com>
2026-05-25 16:56:04 +0200

    aggiunto la somma

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,8 @@
 print("Hello wolrd!")
+
+def somma(x, y):
+    return x + y
+
+#questi due print sono la stessa cosa
+print("Risultato somma: " + somma(1, 2))
+print(f"Risultato somma: {somma(1, 2)}")
