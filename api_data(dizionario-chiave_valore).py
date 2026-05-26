20ef156bfe7a85592ef142ac66e3c2059bcbccf0
Lelouch-zerotwo <bachetti.angelo005@gmail.com>
2026-05-26 14:58:06 +0000

    esercizi che usano dizionari(sfruttando json)

---
+++ api_data(dizionario-chiave_valore).py
@@ -0,0 +1,16 @@
+import json
+
+#primo esercizio
+raw_data = '{"server": "Amazon", "config": {"port": 80, "active": true}}'
+
+data = json.loads(raw_data)
+
+print(data["config"]["port"])
+
+
+#secondo esercizio
+json_social = '[{"id": 1, "tag": "python"}, {"id": 2, "tag": "gaming"}, {"id": 3, "tag": "python"}, {"id": 4, "tag": "webdev"}, {"id": 5, "tag": "python"}, {"id": 6, "tag": "gaming"}]'
+
+conteggio = json.loads(json_social)
+
+#conteggia quanti e quali tag ci sono nel "json" -->"risultato=  python: 3, gaming: 2, webdev: 1"
