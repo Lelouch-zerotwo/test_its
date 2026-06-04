c2896df2e726d1baed4207cf45c1c5940b4d477a
Lelouch-zerotwo <bachetti.angelo005@gmail.com>
2026-06-04 15:43:31 +0000

    python per vedere funzionamento api

--- registro(lista-->(array)).py
+++
@@ -1,9 +0,0 @@
-voti = [4, 9, 5, 7]
-
-#serve ad aggiungere un elemento alla lista
-#.insert(x, y) aggiunge l'elemento y alla posizione x(senza eliminare nessun dato li sposta)
-#.pop(x) serve per diminuire la lista. Rimuove l'elemento "analizzato" (legge l'ultimo elemento della lista)
-voti.append(8)#aggiunge 8 alla coda della lista
-for p in voti:
-    print("Voto registrato: ")
-    print(p)
